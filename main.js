45f1d6a2006096cec33cef99fcfc39334692afac
Terrance Raper <terranceraper@Madisons-MacBook-Air.local>
2020-12-05 09:16:23 -0600

    added java script button for predictions

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const btn = document.querySelector(".btn-toggle");
 btn.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
 });
+
+function show_hide() {
+  if (document.getElementById("appear").style.display == 'none') {
+    document.getElementById("appear").style.display = 'block';
+  }else{
+    document.getElementById("appear").style.display = 'none';
+  }
+}
