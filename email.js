508b95b7caeaa54ecf9551d4beb4edf7bc6f25ea
Terrance Raper <Terrance@Terrances-iMac.local>
2020-12-14 16:39:40 -0600

    added event countdown timer

--- email.js
+++ email.js
@@ -1,11 +1,27 @@
-var templateParams = {
-    name: 'James',
-    notes: 'Check this out!'
-};
- 
-emailjs.send('service_efxho28', 'template_n54josd', templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+
+  (function() {
+    emailjs.init("user_7sO6wXhCRhLTMmLvKG7lJ");
+  })();
+  
+    
+        window.onload = function() {
+            document.getElementById('contact-form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                // generate a five digit number for the contact_number variable
+                this.contact_number.value = Math.random() * 100000 | 0;
+                // these IDs from the previous steps
+                emailjs.sendForm('contact_service', 'contact_form', this)
+                    .then(function() {
+                        console.log('SUCCESS!');
+                    }, function(error) {
+                        console.log('FAILED...', error);
+                    });
+            });
+        }
+
+        function clearForm(){
+            document.getElementById('name').value='';
+            document.getElementById('email').value='';
+            document.getElementById('message').value='';
+          }
+  
--- main.js
+++ main.js
@@ -3,3 +3,36 @@ const btn = document.querySelector(".btn-toggle");
 btn.addEventListener("click", function () {
   document.body.classList.toggle("dark-theme");
 });
+
+function countdown() {
+    var today = new Date();
+    var eventDate = new Date("December 22, 2020 00:00:00");
+
+    var currentTime = today.getTime();
+    var eventTime = eventDate.getTime();
+
+    var remTime = eventTime - currentTime;
+
+    var sec = Math.floor(remTime/1000);
+    var min = Math.floor(sec/60);
+    var hrs = Math.floor(min/60);
+    var days = Math.floor(hrs/24);
+
+    hrs = hrs % 24;
+    min = min % 60;
+    sec = sec % 60;
+
+    hrs = (hrs<10) ? "0" + hrs : hrs;
+    min = (min<10) ? "0" + min : min;
+    sec = (sec<10) ? "0" + sec : sec;
+    
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hrs").innerHTML = hrs;
+    document.getElementById("min").innerHTML = min;
+    document.getElementById("sec").innerHTML = sec;
+
+    setTimeout(countdown, 1000);
+}
+
+countdown();
